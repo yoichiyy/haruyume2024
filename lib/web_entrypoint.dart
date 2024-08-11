763dd77f407b23ca122708a1a4eed6c6617f1447
yoichiyy <yoichiyy@gmail.com>
2024-08-12 07:46:50 +0900

    web_entrypoint削除して、もう一回build した。

--- lib/web_entrypoint.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import '../main.dart' as entrypoint;
-
-void main() {
-  setUrlStrategy(PathUrlStrategy());
-  entrypoint.main();
-}
--- web_entrypoint.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import 'package:myapp/main.dart' as entrypoint;
-
-void main() {
-  setUrlStrategy(PathUrlStrategy());
-  entrypoint.main();
-}
