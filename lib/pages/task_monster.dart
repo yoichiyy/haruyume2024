07b4f852a3ee4508db34e00ffd02a74cca329a9e
yoichiyy <yoichiyy@gmail.com>
2024-09-10 07:28:15 +0900

    monster page

--- lib/pages/task_monster.dart
+++ lib/pages/task_monster.dart
@@ -1,15 +1,10 @@
 import 'package:audioplayers/audioplayers.dart';
-import 'package:counter/task_list/task_model.dart';
-import 'package:counter/task_list/thank_list.dart';
-import 'package:counter/ui/bottom_navigation_bar.dart';
+// import 'package:counter/ui/bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-
-import '../task_edit/create_task.dart';
 
 class TaskMonster extends StatefulWidget {
-  const TaskMonster({required Key key}) : super(key: key);
+  const TaskMonster({super.key});
 
   @override
   State<TaskMonster> createState() => _TaskMonsterState();
@@ -25,18 +20,7 @@ class _TaskMonsterState extends State<TaskMonster> {
   void initState() {
     super.initState();
     _audioPlayer = AudioPlayer();
-
-    final model = Provider.of<TaskModel>(context, listen: false);
-    model.getTodoListRealtime();
   }
-
-  // @override
-  // void dispose() {
-  //   _audioPlayer?.dispose();
-  //   final model = Provider.of<TaskModel>(context, listen: false);
-  //   model.disposeRealtimeListeners();
-  //   super.dispose();
-  // }
 
   void _handleTap() async {
     await _playSound();
@@ -48,11 +32,6 @@ class _TaskMonsterState extends State<TaskMonster> {
       _tapCount++;
       if (_tapCount == 3) {
         _monsterSize = 0.0;
-        //以下の2行は、重複しているがやむなし？
-        final model = Provider.of<TaskModel>(context, listen: false);
-        final todoListForTaskMonster = model.todoListFromModelOverDue;
-
-        model.createTaskForTomorrow(todoListForTaskMonster[0].id);
       } else {
         _monsterSize /= 1.5;
       }
@@ -73,93 +52,37 @@ class _TaskMonsterState extends State<TaskMonster> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: const BottomBar(currentIndex: 0),
+      // bottomNavigationBar: const BottomBar(currentIndex: 0),
       appBar: AppBar(
         title: const Text('やること'),
-        actions: [
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
-            child: OutlinedButton(
-              child:
-                  const Text('THANKS', style: TextStyle(color: Colors.white)),
-              onPressed: () async {
-                await Navigator.push<void>(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ThankList(),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
       ),
       body: Center(
-        child: Consumer<TaskModel>(
-          builder: (context, model, child) {
-            final todoListForTaskMonster = model.todoListFromModelOverDue;
-
-            return model.isLoading
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: todoListForTaskMonster.isEmpty
-                        ? [
-                            const Expanded(
-                              child: Center(
-                                child: Text('おめでとうございます！'),
-                              ),
-                            ),
-                          ]
-                        : [
-                            GestureDetector(
-                              onTap: _handleTap,
-                              child: AnimatedContainer(
-                                duration: const Duration(milliseconds: 500),
-                                curve: Curves.easeInOut,
-                                alignment: Alignment.center,
-                                // transform: Matrix4.identity()
-                                //   ..scale(scaleFactor, scaleFactor),
-                                width: _monsterSize,
-                                height: _monsterSize * 2,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: <Widget>[
-                                    Expanded(
-                                      child: Image.asset('images/shoggoth.png'),
-                                    ),
-                                    Expanded(
-                                      child: Center(
-                                        child: Text(
-                                          todoListForTaskMonster[0]
-                                              .taskNameOfTodoClass,
-                                          style: TextStyle(
-                                              fontSize: _monsterSize / 10),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                  );
-          },
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: _handleTap,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeInOut,
+                alignment: Alignment.center,
+                width: _monsterSize,
+                height: _monsterSize * 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: Image.asset('images/shoggoth.png'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        heroTag: "hero2",
-        child: const Icon(Icons.egg_alt),
-        onPressed: () {
-          Navigator.push<void>(context,
-              MaterialPageRoute(builder: (context) => const CreateTask()));
-        },
       ),
     );
   }
