5e22d145330a3aef9eea1b39bf2312245f1b6eae
yoichiyy <yoichiyy@gmail.com>
2024-09-21 08:28:00 +0900

    ユーザー情報追加

--- lib/widgets/emotion_classifier_screen.dart
+++ lib/widgets/emotion_classifier_screen.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class EmotionClassifierScreen extends StatefulWidget {
   const EmotionClassifierScreen({super.key});
@@ -13,12 +12,34 @@ class EmotionClassifierScreen extends StatefulWidget {
 
 class EmotionClassifierScreenState extends State<EmotionClassifierScreen> {
   final TextEditingController _textController = TextEditingController();
-  final stt.SpeechToText _speechToText = stt.SpeechToText();
-  bool _isListening = false;
   String _classificationResult = ""; // 感情分類結果を表示
+  String _selectedUser = "mama";
+  bool _isButtonPressed = false;
 
-  // Flask APIを呼び出す関数
-  Future<void> sendToFlask(String inputText) async {
+  // ボタンを押した際に呼ばれる関数
+  void _handleSubmit() {
+    setState(() => _isButtonPressed = true); // ボタンが押されたことを反映
+
+    // SnackBarの表示
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text('送信成功！ 結果表示まで5秒ほど待て'),
+        behavior: SnackBarBehavior.floating,
+        duration: const Duration(seconds: 2),
+        margin: EdgeInsets.only(
+            bottom: MediaQuery.of(context).size.height / 2 - 50),
+      ),
+    );
+
+    sendToFlask(_textController.text, _selectedUser);
+
+    // ボタンのアニメーションのための一時的な遅延
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() => _isButtonPressed = false);
+    });
+  }
+
+  Future<void> sendToFlask(String inputText, String userId) async {
     // const String apiUrl = "http://127.0.0.1:5000/analyze"; // ローカル
     const String apiUrl = "https://flask-v9rl.onrender.com/analyze"; // FlasK
 
@@ -27,35 +48,18 @@ class EmotionClassifierScreenState extends State<EmotionClassifierScreen> {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: jsonEncode({"text": inputText}), // テキストを送信
+      body: jsonEncode({"text": inputText, "user_id": userId}),
     );
 
     if (response.statusCode == 200) {
       var result = jsonDecode(response.body);
       setState(() {
-        _classificationResult = result['analysis']; // 結果を保存
+        _classificationResult = result['analysis'];
       });
       debugPrint("感情分類結果: $_classificationResult");
     } else {
       debugPrint("APIエラー: ${response.statusCode}");
-    }
-  }
-
-  // 音声入力を開始する関数
-  void _listen() async {
-    if (!_isListening) {
-      bool available = await _speechToText.initialize();
-      if (available) {
-        setState(() => _isListening = true);
-        _speechToText.listen(onResult: (result) {
-          setState(() {
-            _textController.text = result.recognizedWords;
-          });
-        });
-      }
-    } else {
-      setState(() => _isListening = false);
-      _speechToText.stop();
+      setState(() => _isButtonPressed = false);
     }
   }
 
@@ -66,42 +70,86 @@ class EmotionClassifierScreenState extends State<EmotionClassifierScreen> {
       child: Column(
         children: [
           const SizedBox(height: 20),
-          Stack(
-            alignment: Alignment.centerRight,
+          // ユーザーID選択用のラジオボタン
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(
-                height: 200, // 縦3cm (100px 近い)
-                width: 500, // 横5cm (250px 近い)
+              Radio<String>(
+                value: "mama",
+                groupValue: _selectedUser,
+                onChanged: (String? value) {
+                  setState(() {
+                    _selectedUser = value ?? "mama";
+                  });
+                },
+              ),
+              const Text('まま'),
+              Radio<String>(
+                value: "papa",
+                groupValue: _selectedUser,
+                onChanged: (String? value) {
+                  setState(() {
+                    _selectedUser = value ?? "mama";
+                  });
+                },
+              ),
+              const Text('ぱぱ'),
+              Radio<String>(
+                value: "haru",
+                groupValue: _selectedUser,
+                onChanged: (String? value) {
+                  setState(() {
+                    _selectedUser = value ?? "mama";
+                  });
+                },
+              ),
+              const Text('はる'),
+              Radio<String>(
+                value: "yume",
+                groupValue: _selectedUser,
+                onChanged: (String? value) {
+                  setState(() {
+                    _selectedUser = value ?? "mama";
+                  });
+                },
+              ),
+              const Text('ゆめ'),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Flexible(
                 child: TextField(
                   controller: _textController,
-                  maxLines: 20,
+                  minLines: 1, // 初期の高さ
+                  // maxLines: null, // maxLinesをnullに設定して無制限に拡大
+                  expands: false, // これでTextFieldの高さが動的に伸びる
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
-                    hintText: 'テキストを入力してください',
+                    hintText: "今頭の中にあることを書く。\n余裕があれば【問題 →（希望/理想＆感情）←行動】を書く",
                   ),
                 ),
-              ),
-              IconButton(
-                icon: const Icon(Icons.mic),
-                onPressed: _listen, // 音声入力を開始
               ),
             ],
           ),
           const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              sendToFlask(_textController.text); // 送信ボタン押下時にAPIを呼び出す
-            },
-            child: const Text('そうしん'),
+          AnimatedScale(
+            scale: _isButtonPressed ? 0.9 : 1.0, // ボタンが押されたら縮小
+            duration: const Duration(milliseconds: 100),
+            child: ElevatedButton(
+              onPressed: _handleSubmit,
+              child: const Text('そうしん'),
+            ),
           ),
-          const SizedBox(height: 20), // 結果表示のためのスペース
+          const SizedBox(height: 20),
           Container(
-            padding: const EdgeInsets.all(10.0), // 枠線内の余白
-            margin:
-                const EdgeInsets.symmetric(horizontal: 10.0), // 左右の余白（1cm相当）
+            padding: const EdgeInsets.all(10.0),
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
             decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 2.0), // 黒い枠線
-              borderRadius: BorderRadius.circular(5.0), // 角を少し丸める
+              border: Border.all(color: Colors.black, width: 2.0),
+              borderRadius: BorderRadius.circular(5.0),
             ),
             constraints: const BoxConstraints(
               maxWidth: double.infinity, // 画面横いっぱい
@@ -111,7 +159,8 @@ class EmotionClassifierScreenState extends State<EmotionClassifierScreen> {
               style: const TextStyle(fontSize: 18),
               textAlign: TextAlign.left, // テキストを左揃えに
             ),
-          )
+          ),
+          const SizedBox(height: 100),
         ],
       ),
     );
